0603dc0704253dfaad04bd4757fedff3620e5a0b
HAVenueReveal <hwedsa2026@gmail.com>
2025-07-28 14:22:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
-class VenueReveal {
-    constructor(previewImg, bkgdImg, height, width, caption) {
-        this.previewImg = previewImg;
-        this.bkgdImg = bkgdImg;
-        this.caption = caption;
-        
-        this.container = document.createElement("figure");
-        this.container.setClass("venue");
-
-        this.revealCard = document.createElement("canvas");
-        this.revealCard.setClass("venue-card");
-        this.revealCard.height = height;
-        this.revealCard.width = width;
-
-        this.venueCaption = document.createElement("figcaption");
-        this.venueCaption.innerText = caption;
-
-        this.container.appendChild(revealCard);
-        this.container.appendChild(venueCaption);
-    }
-
-    attach(parent) {
-        parent.appendChild(this.container);
-    }
-}
-// canvas element
-var bridge = document.getElementById("bridge"),
-bridgeCanvas = bridge.getContext('2d'),
-brushRadius = (bridge.width / 100) * 5,
-img = new Image();
-
-if (brushRadius < 50) { brushRadius = 50 }
-
-img.onload = function(){  
-	bridgeCanvas.drawImage(img, 0, 0, bridge.width, bridge.height);
-}
-img.src = 'ReceptionCover.png';
-
-function detectLeftButton(event) {
+function isLeftClick(event) {
     if ('buttons' in event) {
         return event.buttons === 1;
     } else if ('which' in event) {
@@ -46,35 +8,96 @@ function detectLeftButton(event) {
     }
 }
 
-function getBrushPos(xRef, yRef) {
-	var bridgeRect = bridge.getBoundingClientRect();
-	return {
-	  x: Math.floor((xRef-bridgeRect.left)/(bridgeRect.right-bridgeRect.left)*bridge.width),
-	  y: Math.floor((yRef-bridgeRect.top)/(bridgeRect.bottom-bridgeRect.top)*bridge.height)
-    };
-}
-      
-function drawDot(mouseX,mouseY){
-	bridgeCanvas.beginPath();
-    bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2*Math.PI, true);
-    bridgeCanvas.fillStyle = '#000';
-    bridgeCanvas.globalCompositeOperation = "destination-out";
-    bridgeCanvas.fill();
+class VenueReveal {
+    constructor(previewImg, bkgdImg, height, width, caption) {
+        const self = this;
+        
+        this.container = document.createElement('figure');
+        this.container.setClass('venue');
+
+        this.revealCard = document.createElement('canvas');
+        this.revealCard.setClass('venue-card');
+        this.revealCard.style.backgroundImage = `url('${bkgdImg}')`;
+        this.revealCard.height = height;
+        this.revealCard.width = width;
+        this.container.appendChild(revealCard);
+
+        if (caption) {
+            this.venueCaption = document.createElement('figcaption');
+            this.venueCaption.innerText = caption;
+            this.container.appendChild(venueCaption);
+        }
+
+        // can try changing this brush size
+        this.brushRadius = Math.max(50, (this.revealCard.width / 100) * 5);
+        
+        const coverImage = new Image();
+        coverImage.src = previewImg;
+        coverImage.onload = function() {  
+            let bridgeCanvas = self.revealCard.getContext('2d');
+        	bridgeCanvas.drawImage(coverImage, 0, 0, self.revealCard.width, self.revealCard.height);
+        }
+
+        this.revealCard.addEventListener('mousemove', function(e) {
+            if (isLeftClick(e)) {
+                let brushPos = self.getBrushPos(e.clientX, e.clientY);
+                self.drawDot(brushPos.x, brushPos.y);
+            }
+        }, false);
+        
+        this.revealCard.addEventListener('touchmove', function(e) {
+            e.preventDefault();
+            let touch = e.targetTouches[0];
+            if (touch) {
+                let brushPos = self.getBrushPos(touch.pageX, touch.pageY);
+                self.drawDot(brushPos.x, brushPos.y);
+            }
+        }, false);
+    }
+
+    attach(parent) {
+        parent.appendChild(this.container);
+    }
+    
+    getBrushPos(xRef, yRef) {
+        let bridgeRect = this.revealCard.getBoundingClientRect();
+        return {
+            x: Math.floor((xRef-bridgeRect.left)/(bridgeRect.right-bridgeRect.left) * this.revealCard.width),
+            y: Math.floor((yRef-bridgeRect.top)/(bridgeRect.bottom-bridgeRect.top) * this.revealCard.height)
+        };
+    }
+          
+    drawDot(x, y) {
+        let bridgeCanvas = this.revealCard.getContext('2d');
+    	bridgeCanvas.beginPath();
+        bridgeCanvas.arc(x, y, this.brushRadius, 0, 2 * Math.PI, true);
+        bridgeCanvas.fillStyle = '#000';
+        bridgeCanvas.globalCompositeOperation = 'destination-out';
+        bridgeCanvas.fill();
+    }
 }
 
-bridge.addEventListener("mousemove", function(e) {
-    var leftBut = detectLeftButton(e);
-    if (leftBut == 1) {
-        var brushPos = getBrushPos(e.clientX, e.clientY);
-        drawDot(brushPos.x, brushPos.y);
-    }
-}, false);
+const venues = [
+    new VenueReveal(
+        'ReceptionCover.png',
+        'FortedeCruzReveal.jpg',
+        465, 750,
+        'Hansa & Ajit Wedding Venue Reveal - Scratch to find out!'
+    ),
+    new VenueReveal(
+        'ReceptionCover.png',
+        'FortedeCruzReveal.jpg',
+        465, 750,
+        'Test 2'
+    ),
+    new VenueReveal(
+        'ReceptionCover.png',
+        'FortedeCruzReveal.jpg',
+        465, 750
+    )
+];
 
-bridge.addEventListener("touchmove", function(e) {
-    e.preventDefault();
-    var touch = e.targetTouches[0];
-    if (touch) {
-        var brushPos = getBrushPos(touch.pageX, touch.pageY);
-        drawDot(brushPos.x, brushPos.y);
-    }
-}, false);
+addEventListener('DOMContentLoaded', (event) => {
+    const venueContainer = document.getElementById('venue-container');
+    venues.forEach((venue) => venue.attach(venueContainer));
+});
